883cb2fcbac673732f5786232b40c6990362d36c
Akobir5 <toshtemirovakobir8@gmail.com>
2022-01-14 15:57:12 +0300

    add json info

---
+++ JS/index.js
@@ -0,0 +1,9 @@
+let headerH = document.querySelector('.headerH')
+
+window.addEventListener('scroll', (e) => {
+    if(scrollY > 150) {
+        headerH.classList.add('headerScroll')
+    } else {
+        headerH.classList.remove('headerScroll')
+    }
+})
