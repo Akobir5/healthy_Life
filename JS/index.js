a425f219ef75dff0b1571d1e3440f39f42913ac2
Akobir5 <toshtemirovakobir8@gmail.com>
2022-01-24 08:40:58 +0300

    add 1 style

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,6 @@
 let headerH = document.querySelector('.headerH')
-
-
+let menu_Icon = document.querySelectorAll('.menu_Icon')
+let card = document.querySelectorAll('.card')
 
 window.addEventListener('scroll', (e) => {
     if(scrollY > 150) {
@@ -9,3 +9,11 @@ window.addEventListener('scroll', (e) => {
         headerH.classList.remove('headerScroll')
     }
 })
+
+menu_Icon.forEach((item, index) => {
+    item.addEventListener('click', () => {   
+            card[index].classList.toggle('humburger_checked')
+    })
+})
+
+
