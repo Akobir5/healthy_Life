6e2faef49767d5d14e6f91fdb0cbf565684d3f31
Akobir5 <toshtemirovakobir8@gmail.com>
2022-02-04 08:14:56 +0300

    animate nav

--- JS/animate.js
+++ JS/animate.js
@@ -1,17 +1,26 @@
 let menu_home = document.querySelector('.menu_home')
 let menu_box = document.querySelector('.menu_box')
+let toggleBlock = document.querySelector('.toggleBlock')
+let allHeaderItems = document.querySelectorAll('#header li')
 
-menu_home.addEventListener('click', (e) => {
+menu_home.addEventListener('click', () => {
     menu_box.classList.toggle('menu_box_check')
-
-    console.log(e);
+    toggleBlock.classList.toggle('menu_background')
 })
 
-// window.addEventListener('click', () => {
-//     if(menu_box.classList.contains('menu_box_check')) {
-//         menu_box.classList.remove('menu_box_check')
-//     } else {
-//         menu_box.classList.add('menu_box_check')
-//     }
-// })
+toggleBlock.addEventListener('click', () => {
+    menu_box.classList.remove('menu_box_check')
+    toggleBlock.classList.remove('menu_background')
+})
 
+allHeaderItems.forEach(items => {
+    items.addEventListener('click', () => {
+        if (window.getSelection) {
+            window.getSelection().removeAllRanges();
+          } else { 
+            document.selection.empty();
+          }
+    })
+})
+
+console.log("hello");
